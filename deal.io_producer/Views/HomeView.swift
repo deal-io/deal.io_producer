84c4629f634937a3f70b968fbf4f961245119bd7
Tyler Keller <96822088+tylerckeller@users.noreply.github.com>
2023-03-13 13:50:48 -0700

    Tylerkeller active connect (#10)
    * everything builds but not everything works as expected
    
    * added skeleton code for the API calls

--- deal.io_producer/Views/HomeView.swift
+++ deal.io_producer/Views/HomeView.swift
@@ -12,26 +12,24 @@ struct HomeView: View {
     @State var newDealToggle = false
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("dealio_white_on_bg")
-                    .resizable()
-                    .frame(width: 100, height: 50)
-                    .padding(.vertical, 10)
-                FeedView(homeVM: homeVM)
-                Spacer()
-                PlusButtonView()
-                    .onTapGesture {
-                        newDealToggle = true
-                    } .sheet(isPresented: $newDealToggle) {
-                        PostCreationView(dealVM: homeVM.generateNewDealViewModel(restaurant: homeVM.getRestaurant()))
-                            .background(Deal_ioColor.background)
-                    }
-                    .padding(.top, 10)
-            }
-            .background(Deal_ioColor.background.edgesIgnoringSafeArea(.all))
+        VStack {
+            Image("dealio_white_on_bg")
+                .resizable()
+                .frame(width: 100, height: 50)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 150)
+            FeedView(homeVM: homeVM)
+            Spacer()
+            PlusButtonView()
+                .onTapGesture {
+                    newDealToggle = true
+                } .sheet(isPresented: $newDealToggle) {
+                    PostCreationView(dealVM: homeVM.generateNewDealViewModel(restaurant: homeVM.getRestaurant()))
+                        .background(Deal_ioColor.background)
+                }
+                .padding(.top, 10)
         }
-        .background(Deal_ioColor.background.edgesIgnoringSafeArea(.all))
+        .background(Deal_ioColor.background)
     }
 }
 
