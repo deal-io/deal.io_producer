9643f231b09c99c5ca4a9a2f2754dac3d8445baa
Tyler Keller <tylerkeller@mines.edu>
2023-03-25 13:02:20 -0600

    added firebase config file and updated login view

--- deal.io_producer/Deal_ioProducerApp.swift
+++ deal.io_producer/Deal_ioProducerApp.swift
@@ -6,10 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 
 @main
 struct Deal_ioProducerApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
