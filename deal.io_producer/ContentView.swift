221abf525efb578bc4511a9b820e352914fb7783
Levi <leviortega1234@gmail.com>
2023-03-26 10:06:03 -0600

    Fixed login issue
    Need to rebase to fix creating deals

--- deal.io_producer/ContentView.swift
+++ deal.io_producer/ContentView.swift
@@ -11,10 +11,9 @@ import FirebaseAuth
 
 struct ContentView: View {
     @StateObject var authState = AuthState()
-    @State var userLoading = true
 
     var body: some View {
-        if (authState.isLoggedIn && userLoading == false) {
+        if (authState.isLoggedIn && authState.userInfoLoaded == true) {
             HomeView(viewModel: ProducerViewModel(restaurant: authState.restaurant!))
         } else {
             LoginView(authState: authState)
@@ -23,33 +22,38 @@ struct ContentView: View {
 }
 
 class AuthState: ObservableObject {
+    private var LOG_TAG = "LOG: AuthState"
+    
     @Published var isLoggedIn = false
-    @Published var user: User? = nil
+    @Published var userInfoLoaded = false
     @Published var user: User? = nil
     @Published var restaurant: Restaurant? = nil
     var mDealService = DealService()
 
     init() {
-        // Check the user's authentication state when the app starts
-        Auth.auth().addStateDidChangeListener { auth, user in
-            if let user = user {
-                // User is signed in
-                self.isLoggedIn = true
-                self.user = user
-                self.getUser()
-                
-            } else {
-                // User is not signed in
-                self.isLoggedIn = false
-                self.user = nil
-                self.user = nil
-                self.restaurant = nil
+        
+        
+            // Check the user's authentication state when the app starts
+            Auth.auth().addStateDidChangeListener { auth, user in
+                if let user = user {
+                    // User is signed in
+                    print("\(self.LOG_TAG) Successful Login")
+                    self.isLoggedIn = true
+                    self.getUser(userUID: user.uid)
+                    
+                } else {
+                    // User is not signed in
+                    self.isLoggedIn = false
+                    self.user = nil
+                    self.restaurant = nil
+                }
             }
-        }
+        
+      
     }
     
-    func getUser() {
-        mDealService.fetchUser(uid: user!.uid) { result in
+    func getUser(userUID: String) {
+        mDealService.fetchUser(uid: userUID) { result in
                switch result {
                case .success(let user):
                    DispatchQueue.main.async {
@@ -70,7 +74,9 @@ class AuthState: ObservableObject {
             case .success(let restaurant):
                 DispatchQueue.main.async {
                     self.restaurant = restaurant
+                    print("\(self.LOG_TAG) Successful Info Fetch \nRID: \(restaurant.id)")
                     print("\(restaurant.id)")
+                    self.userInfoLoaded = true
                 }
                 
             case .failure(let error):
