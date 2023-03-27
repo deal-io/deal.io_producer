31f2426f97b340359ca589af0ae3268fccf7a5cc
Tyler Keller <tylerkeller@mines.edu>
2023-03-26 18:31:29 -0600

    hour view works for all deals except MultiPickers w/o current date selected

--- deal.io_producer/Views/Assets/HourView.swift
+++ deal.io_producer/Views/Assets/HourView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct HourView: View {
+    let LOG_TAG = "HOUR_VIEW: "
+    
     @ObservedObject var viewModel: ProducerViewModel
     var deal: Deal
     var startTime: String
     var endTime: String
+    var startHourDifference: Int
+    var endHourDifference: Int
     var upcomingDay: String
-    var hourDifference: Int
     var active: Bool
 
     
@@ -22,38 +25,47 @@ struct HourView: View {
         self.deal = deal
         self.startTime = deal.dealAttributes.startTime
         self.endTime = deal.dealAttributes.endTime
+        self.startHourDifference = DateUtil().getHourDifference(inputHour: startTime)
+        self.endHourDifference = DateUtil().getHourDifference(inputHour: endTime)
         self.upcomingDay = DateUtil().getFirstActiveWeekday(daysActive: deal.dealAttributes.daysActive)!
-        self.hourDifference = DateUtil().getHourDifference(inputHour: endTime)
         self.active = deal.dealAttributes.daysActive[0]
     }
     
     var body: some View {
         VStack {
             if (active) {
-                if hourDifference < 0 {
-                    Text("EXPIRED")
+                if startHourDifference > 0 {
+                    Text("\(startTime)")
                         .padding(8)
-                        .background(Deal_ioColor.oneHourColor)
+                        .background(Deal_ioColor.upcomingColor)
                         .foregroundColor(Color.white)
                         .cornerRadius(10)
-                } else if hourDifference <= 1 {
-                    Text("< 1 hr")
-                        .padding(8)
-                        .background(Deal_ioColor.oneHourColor)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(10)
-                } else if hourDifference <= 2 {
-                    Text("\(hourDifference) hrs")
-                        .padding(8)
-                        .background(Deal_ioColor.twoHourColor)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(10)
-                } else if hourDifference > 2 {
-                    Text("\(hourDifference) hrs")
-                        .padding(8)
-                        .background(Deal_ioColor.fourHourColor)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(10)
+                } else {
+                    if endHourDifference < 0 {
+                        Text("EXPIRED")
+                            .padding(8)
+                            .background(Deal_ioColor.oneHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    } else if endHourDifference <= 1 {
+                        Text("< 1 hr")
+                            .padding(8)
+                            .background(Deal_ioColor.oneHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    } else if endHourDifference <= 2 {
+                        Text("\(endHourDifference) hrs")
+                            .padding(8)
+                            .background(Deal_ioColor.twoHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    } else if endHourDifference > 2 {
+                        Text("\(endHourDifference) hrs")
+                            .padding(8)
+                            .background(Deal_ioColor.fourHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    }
                 }
             } else {
                 Text("\(upcomingDay) at \(endTime)")
