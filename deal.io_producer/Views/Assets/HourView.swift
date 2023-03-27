b562ef037c43da1fc975e75bea8387f7ad759c11
Tyler Keller <96822088+tylerckeller@users.noreply.github.com>
2023-03-26 17:34:34 -0700

    HourView implementation (#30)
    * added alerts for invalid daysActive and invalid Start and End Times
    
    * all active deal hour views work
    
    * hour view works for all deals except MultiPickers w/o current date selected

--- deal.io_producer/Views/Assets/HourView.swift
+++ deal.io_producer/Views/Assets/HourView.swift
@@ -8,45 +8,67 @@
 import SwiftUI
 
 struct HourView: View {
+    let LOG_TAG = "HOUR_VIEW: "
+    
     @ObservedObject var viewModel: ProducerViewModel
     var deal: Deal
-    var startDate: Date
-    var endDate: Date
-    var daily: Bool
+    var startTime: String
+    var endTime: String
+    var startHourDifference: Int
+    var endHourDifference: Int
+    var upcomingDay: String
+    var active: Bool
 
     
     init(viewModel: ProducerViewModel, deal: Deal) {
         self.viewModel = viewModel
         self.deal = deal
-        self.daily = deal.dealAttributes.daysActive[0]
-        self.startDate = DateUtil().secondsToDate(seconds: deal.dealAttributes.startDate._seconds, nanoseconds: deal.dealAttributes.startDate._nanoseconds)
-        self.endDate = DateUtil().secondsToDate(seconds: deal.dealAttributes.endDate._seconds, nanoseconds: deal.dealAttributes.endDate._nanoseconds)
+        self.startTime = deal.dealAttributes.startTime
+        self.endTime = deal.dealAttributes.endTime
+        self.startHourDifference = DateUtil().getHourDifference(inputHour: startTime)
+        self.endHourDifference = DateUtil().getHourDifference(inputHour: endTime)
+        self.upcomingDay = DateUtil().getFirstActiveWeekday(daysActive: deal.dealAttributes.daysActive)!
+        self.active = deal.dealAttributes.daysActive[0]
     }
     
     var body: some View {
         VStack {
-            if (deal.dealAttributes.daysActive[0]) {
-                if (DateUtil().diffCurrentDateToInputDate(date: endDate)) <= 1 {
-                    Text("<\(DateUtil().diffCurrentDateToInputDate(date: endDate)) hr")
+            if (active) {
+                if startHourDifference > 0 {
+                    Text("\(startTime)")
                         .padding(8)
-                        .background(Deal_ioColor.oneHourColor)
+                        .background(Deal_ioColor.upcomingColor)
                         .foregroundColor(Color.white)
                         .cornerRadius(10)
-                } else if DateUtil().diffCurrentDateToInputDate(date: endDate) <= 2 {
-                    Text("\(DateUtil().diffCurrentDateToInputDate(date: endDate)) hrs")
-                        .padding(8)
-                        .background(Deal_ioColor.twoHourColor)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(10)
-                } else if DateUtil().diffCurrentDateToInputDate(date: endDate) > 2 {
-                    Text("\(DateUtil().diffCurrentDateToInputDate(date: endDate)) hrs")
-                        .padding(8)
-                        .background(Deal_ioColor.fourHourColor)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(10)
+                } else {
+                    if endHourDifference < 0 {
+                        Text("EXPIRED")
+                            .padding(8)
+                            .background(Deal_ioColor.oneHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    } else if endHourDifference <= 1 {
+                        Text("< 1 hr")
+                            .padding(8)
+                            .background(Deal_ioColor.oneHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    } else if endHourDifference <= 2 {
+                        Text("\(endHourDifference) hrs")
+                            .padding(8)
+                            .background(Deal_ioColor.twoHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    } else if endHourDifference > 2 {
+                        Text("\(endHourDifference) hrs")
+                            .padding(8)
+                            .background(Deal_ioColor.fourHourColor)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    }
                 }
             } else {
-                Text(DateUtil().formattedHourComponentFromDate(date: startDate))
+                Text("\(upcomingDay) at \(endTime)")
                     .padding(8)
                     .background(Deal_ioColor.upcomingColor)
                     .foregroundColor(Color.white)
